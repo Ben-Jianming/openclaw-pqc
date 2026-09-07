b0ded122962ba0a557ed7d26d95c6c296dc11e5c
Ben-Jianming <v5dtw7vkms@privaterelay.appleid.com>
2026-09-07 23:54:31 +0800

    fix(types): declare Noble crypto dependencies

--- src/infra/crypto/provider-noble.ts
+++ src/infra/crypto/provider-noble.ts
@@ -1,6 +1,6 @@
 import { ed25519 } from "@noble/curves/ed25519.js";
-import { hmac as hmacN } from "@noble/hashes/hmac";
-import { sha256, sha384, sha512 } from "@noble/hashes/sha2";
+import { hmac as hmacN } from "@noble/hashes/hmac.js";
+import { sha256, sha384, sha512 } from "@noble/hashes/sha2.js";
 import { ml_dsa44, ml_dsa65, ml_dsa87 } from "@noble/post-quantum/ml-dsa.js";
 import { ml_kem768 } from "@noble/post-quantum/ml-kem.js";
 import { CryptoError, UnsupportedAlgorithmError } from "./provider.js";
